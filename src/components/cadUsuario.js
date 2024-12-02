bcf2bff1c3488c426bfb000ecbfb4e4a23fb8dc5
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2024-12-02 12:49:06 -0300

    fim

--- src/components/cadUsuario.js
+++ src/components/cadUsuario.js
@@ -77,7 +77,7 @@ const CadastroUsuario = () => {
         {usuarios.map((user) => (
           <li key={user.id}>
             <img src={user.urlAvatar} style={{ width: 50 }} />
-            <td>{user.nickname}</td>
+            <h4>{user.nickname}</h4>
             <td>
               <Button onClick={()=>{handleEdit(user)}
               }variant="warning">
@@ -93,7 +93,6 @@ const CadastroUsuario = () => {
                       <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                   </svg>           
               </Button>
-             
           </td>
           <hr></hr>
           </li>
