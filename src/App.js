31edc3a33c824c17ed533ab018bb2cd082242594
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2024-12-02 12:34:54 -0300

    fim

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<TelaPrincipal />} /> 
+        <Route path="/telaPrincipal" element={<TelaPrincipal />} /> 
         <Route path="/usuarios" element={<CadastroUsuario />} />
         <Route path="/chat" element={<CHAT />} /> 
       </Routes>
