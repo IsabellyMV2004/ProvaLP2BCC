bfc527997de5480105f78bf89d63d39245fcb906
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2024-12-02 12:42:04 -0300

    fim

--- src/redux/MensagensRedux.js
+++ src/redux/MensagensRedux.js
@@ -2,23 +2,23 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const BASE_URL = 'https://backend-bcc-2-b.vercel.app/mensagem';
-
-export const buscarMensagens = createAsyncThunk('messages/buscarMensagens', async () => {
+export const buscarMensagens = createAsyncThunk('messages/buscarMensagens', async () => 
+{
   const response = await axios.get(BASE_URL);
   return response.data.listaMensagens; 
 });
-
-export const enviarMensagem = createAsyncThunk('messages/enviarMensagem', async (mensagem) => {
+export const enviarMensagem = createAsyncThunk('messages/enviarMensagem', async (mensagem) => 
+{
   const response = await axios.post(BASE_URL, mensagem);
   return response.data; 
 });
-
-export const atualizarStatusMensagem = createAsyncThunk('messages/atualizarStatusMensagem', async (id) => {
+export const atualizarStatusMensagem = createAsyncThunk('messages/atualizarStatusMensagem', async (id) => 
+{
   const response = await axios.patch(`${BASE_URL}/${id}`, { lida: true });
   return response.data;
 });
-
-export const excluirMensagem = createAsyncThunk('messages/excluirMensagem', async (id) => {
+export const excluirMensagem = createAsyncThunk('messages/excluirMensagem', async (id) => 
+{
   const response = await axios.delete(`${BASE_URL}/${id}`);
   return response.data;
 });
@@ -27,23 +27,18 @@ const messageSlice = createSlice({
   name: 'messages',
   initialState: [],
   reducers: {},
-  extraReducers: (builder) => {
+  extraReducers: (builder) =>
+  {
     builder
-      .addCase(buscarMensagens.fulfilled, (state, action) => {
-        return action.payload; 
-      })
-      .addCase(enviarMensagem.fulfilled, (state, action) => {
-        state.push(action.payload); 
-      })
+      .addCase(buscarMensagens.fulfilled, (state, action) => { return action.payload;})
+      .addCase(enviarMensagem.fulfilled, (state, action) => { state.push(action.payload);})
       .addCase(atualizarStatusMensagem.fulfilled, (state, action) => {
         const index = state.findIndex((message) => message.id === action.payload.id);
         if (index !== -1) {
           state[index].lida = true; 
         }
       })
-      .addCase(excluirMensagem.fulfilled, (state, action) => {
-        return state.filter((message) => message.id !== action.payload.id);
-      });
+      .addCase(excluirMensagem.fulfilled, (state, action) => { return state.filter((message) => message.id !== action.payload.id);});
   },
 });
 
--- src/redux/controleUsuario.js
+++ src/redux/controleUsuario.js
@@ -2,52 +2,44 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const BASE_URL = 'https://backend-bcc-2-b.vercel.app/usuario';
-
-export const buscarUsuarios = createAsyncThunk('usuarios/buscarUsuarios', async () => {
+export const buscarUsuarios = createAsyncThunk('usuarios/buscarUsuarios', async () => 
+{
   const response = await axios.get(BASE_URL);
   return response.data.listaUsuarios;  
 });
-
-export const adicionarUsuario = createAsyncThunk('usuarios/adicionarUsuario', async (usuario) => {
+export const adicionarUsuario = createAsyncThunk('usuarios/adicionarUsuario', async (usuario) => 
+{
   const response = await axios.post(BASE_URL, usuario);
   return response.data; 
 });
-
-export const atualizarUsuario = createAsyncThunk('usuarios/atualizarUsuario', async (usuario) => {
+export const atualizarUsuario = createAsyncThunk('usuarios/atualizarUsuario', async (usuario) => 
+{
   const response = await axios.put(BASE_URL, usuario);
   return response.data;
 });
-
-export const excluirUsuario = createAsyncThunk('usuarios/excluirUsuario', async (usuario) => {
+export const excluirUsuario = createAsyncThunk('usuarios/excluirUsuario', async (usuario) =>
+{
   const response = await axios.delete(BASE_URL, { data: usuario });
   return response.data;
 });
-
 const controleUsuario = createSlice({
   name: 'usuarios',
   initialState: { list: [], activeUser: null },
   reducers: {
-    setUsuarioAtivo: (state, action) => {
-      state.activeUser = action.payload;
-    },
+    setUsuarioAtivo: (state, action) => {state.activeUser = action.payload;},
   },
-  extraReducers: (builder) => {
+  extraReducers: (builder) => 
+  {
     builder
-      .addCase(buscarUsuarios.fulfilled, (state, action) => {
-        state.list = action.payload;  
-      })
-      .addCase(adicionarUsuario.fulfilled, (state, action) => {
-        state.list.push(action.payload); 
-      })
+      .addCase(buscarUsuarios.fulfilled, (state, action) => {state.list = action.payload;  })
+      .addCase(adicionarUsuario.fulfilled, (state, action) => { state.list.push(action.payload); })
       .addCase(atualizarUsuario.fulfilled, (state, action) => {
         const index = state.list.findIndex(user => user.id === action.payload.id);
         if (index !== -1) {
           state.list[index] = action.payload; 
         }
       })
-      .addCase(excluirUsuario.fulfilled, (state, action) => {
-        state.list = state.list.filter(user => user.id !== action.payload.id);
-      });
+      .addCase(excluirUsuario.fulfilled, (state, action) => {state.list = state.list.filter(user => user.id !== action.payload.id);});
   },
 });
 
